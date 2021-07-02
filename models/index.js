df366e6c944d20bdf0940e28af67025837b08bc4
Sharon <sharonelizalex@gmail.com>
2021-07-02 19:55:18 +1000

    added table associations

--- models/index.js
+++ models/index.js
@@ -1,44 +1,31 @@
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
 
-const { Model, DataTypes } = require("sequelize");
-const sequelize = require("../config/connection");
-class Comment extends Model {}
+User.hasMany(Post, {
+    foreignKey: "user_id",
+  });
 
-Comment.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    comment_text: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: { len: [1] }, 
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: "user",
-        key: "id",
-      },
-    },
-    post_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: "post",
-        key: "id",
-      },
-    },
-  },
-  {
-    sequelize,
-    freezeTableName: true,
-    underscored: true,
-    modelName: "comment",
-  }
-);
+User.hasMany(Comment, {
+    foreignKey: "user_id",
+  });
 
-module.exports = Comment;
+  Post.belongsTo(User, {
+    foreignKey: "user_id",
+  });
+  
+  Post.hasMany(Comment, {
+    foreignKey: "post_id",
+  });
+
+  Comment.belongsTo(User, {
+    foreignKey: "user_id",
+  });
+
+  Comment.belongsTo(Post, {
+    foreignKey: "post_id",
+  });
+
+  module.exports = { User, Post, Comment };
+  
+  
