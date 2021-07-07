0e69cc1bf6a64ab244853b038d4c4c5553fa98d4
Sharon <sharonelizalex@gmail.com>
2021-07-07 16:22:59 +1000

    added comment route for post and put

--- controllers/api/comment-route.js
+++ controllers/api/comment-route.js
@@ -24,5 +24,38 @@ router.get('/:id', (req, res) => {
 
 
 
+router.post('/', withAuth, (req, res) => {
+    if (req.session) {
+        Comment.create({
+                comment_text: req.body.comment_text,
+                post_id: req.body.post_id,
+                user_id: req.session.user_id,
+            })
+            .then(comment => res.json(comment))
+            .catch(err => {
+                res.status(500).json(err);
+            })
+    }
+});
+
+router.put('/:id', withAuth, (req, res) => {
+    Comment.update({
+        comment_text: req.body.comment_text
+    }, {
+        where: {
+            id: req.params.id
+        }
+    }).then(comment => {
+        if (!comment) {
+            res.status(404).json({ message: 'No comment found with this id' });
+            return;
+        }
+        res.json(comment);
+    }).catch(err => {
+        res.status(500).json(err);
+    });
+});
+
+
 
 
