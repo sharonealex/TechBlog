b149bc3cf1ee5bcf5c5c869f7d5333f1780adacd
Sharon <sharonelizalex@gmail.com>
2021-07-07 16:19:36 +1000

    added comments route for get all and by id

---
+++ controllers/api/comment-route.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.get('/', (req, res) => {
+    Comment.findAll({})
+        .then(comments => res.json(comments))
+        .catch(err => {
+            res.status(500).json(err);
+        })
+});
+
+router.get('/:id', (req, res) => {
+    Comment.findAll({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(comments => res.json(comments))
+        .catch(err => {
+            res.status(500).json(err);
+        })
+});
+
+
+
+
+
